69fefe31348fd6798458463f0b1e9248c520f120
giaplv57 <giaplv_57@vnu.edu.vn>
2016-04-15 12:41:48 -0700

    command injection detection now support shell_exec('cat '.['filename'])

--- Test/commandInjection.php
+++ Test/commandInjection.php
@@ -6,6 +6,7 @@
  * Time: 2:23 AM
  */
 
+//------------TEST1 - PASSED----------------//
 //function purify1($string, $con, $dummy){
 //    return htmlentities($string);
 //}
@@ -17,9 +18,16 @@
 //$a = purify1(($_GET['abc']), 15, 123);
 //passthru($a);
 //passthru(purify2($_POST['abc']));
+//------------ENDTEST1----------------//
 
-print("Please specify the name of the file to delete");
-print("<p>");
-$file=$_GET['filename'];
-system("rm $file");
+//------------TEST2 - PASSED----------------//
+//print("Please specify the name of the file to delete");
+//print("<p>");
+//$file=$_GET['filename'];
+//passthru("rm $file");
+//------------ENDTEST2----------------//
+
+//------------TEST3 - UNPASSED----------------//
+echo shell_exec('cat '.$_GET['filename']);
+//------------ENDTEST3----------------//
 ?>
