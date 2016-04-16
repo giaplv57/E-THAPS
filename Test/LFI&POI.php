c7458add04319cc67fc159ef870009de64212545
giaplv57 <giaplv_57@vnu.edu.vn>
2016-04-16 14:44:26 -0700

    LFI and POI dectection now support return from user defined functions

--- Test/LFI&POI.php
+++ Test/LFI&POI.php
@@ -5,12 +5,66 @@
  * Date: 4/15/16
  * Time: 10:47 PM
  */
-//class PHPParser_Node_Expr_Include extends PHPParser_Node_Expr
-//{
-//    const TYPE_INCLUDE      = 1;
-//    const TYPE_INCLUDE_ONCE = 2;
-//    const TYPE_REQUIRE      = 3;
-//    const TYPE_REQUIRE_ONCE = 4;
 
-include(escapeshellcmd(htmlentities($_POST['id'])));
-require_once addslashes(htmlentities($_POST['id']));
+//------------TEST1 - PASSED----------------//
+include(escapeshellcmd(htmlentities($_POST['id']))); #VUL
+require_once addslashes(htmlentities($_POST['id'])); #VUL
+include(pathinfo(htmlentities($_POST['id']))); #NOT VUL
+$user_data = unserialize($_GET['data']); #VUL
+
+function purify1($string, $con, $dummy){
+    return addslashes($string);
+}
+function purify2($string, $con, $dummy){
+    return basename($string);
+}
+include purify1(($_GET['abc']), 15, 123); #VUL
+require purify2(($_GET['cde']), 15, 123); #NOT VUL
+//------------ENDTEST1----------------//
+
+
+//------------TEST2 - PASSED----------------//
+//print("Please specify the name of the file to delete");
+//print("<p>");
+//$file=$_GET['filename'];
+//passthru("rm $file");
+//------------ENDTEST2----------------//
+
+//------------TEST3 - PASSED----------------//
+//echo shell_exec('cat '.$_GET['filename']);
+//echo shell_exec('cat '.escapeshellcmd($_GET['filename']));
+//------------ENDTEST3----------------//
+
+//------------TEST4 - PASSED----------------//
+//$a = 'cat';
+//$a .= $_GET['filename'];
+//echo shell_exec($a);
+//$b = 'cat';
+//$b .= escapeshellcmd($_GET['filename']);
+//echo shell_exec($b);
+//
+//echo shell_exec('cat "'.$_GET['command']).'"';
+//------------ENDTEST4----------------//
+
+//------------TEST5 - PASSED----------------//
+//$host = 'google';
+//if (isset( $_GET['host'] ) )
+//    $host = $_GET['host'];
+//system("nslookup " . $host);
+//------------ENDTEST5----------------//
+
+//------------TEST6 - PASSED----------------//
+//$a = $_POST["a"];
+//$b = "";
+//system(testFunc($a));
+//system(testFunc($b));
+//system(cleanFunc($a));
+//test($a);
+//function testFunc($arg) {
+//    echo $arg;
+//    return $arg;
+//}
+//function cleanFunc($arg) {
+//    return escapeshellarg($arg);
+//}
+//------------ENDTEST6----------------//
