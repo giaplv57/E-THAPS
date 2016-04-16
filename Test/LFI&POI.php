635b266074e1730c5a22d012d27ffb40d3971c4e
giaplv57 <giaplv_57@vnu.edu.vn>
2016-04-16 14:14:51 -0700

    LFI detection mechanism now support include and require family functions

--- Test/LFI&POI.php
+++ Test/LFI&POI.php
@@ -5,19 +5,12 @@
  * Date: 4/15/16
  * Time: 10:47 PM
  */
-# include("menu.php");
-#include 'menu.php';
-//include ($_GET['id'].".php");
+//class PHPParser_Node_Expr_Include extends PHPParser_Node_Expr
+//{
+//    const TYPE_INCLUDE      = 1;
+//    const TYPE_INCLUDE_ONCE = 2;
+//    const TYPE_REQUIRE      = 3;
+//    const TYPE_REQUIRE_ONCE = 4;
 
-unserialize($_POST['id']);
-//$F_FILE_INCLUDE = array(
-//    'include' 						, #Param1
-//    'include_once' 					, #Param1
-//    'parsekit_compile_file'			, #Param1: parsekit_compile_file ( string $filename [, array &$errors [, int $options = PARSEKIT_QUIET ]] )
-//    'require' 						, #Param1
-//    'require_once' 					, #Param1
-//    'set_include_path' 				, #Param1: set_include_path ( string $new_include_path )
-//    'virtual' 						, #Param1: virtual ( string $filename )
-//    //    'runkit_import'					, #=> array(array(1), $F_SECURING_FILE),
-//    //    'php_check_syntax' 				, #=> array(array(1), $F_SECURING_FILE),
-//);
+include(escapeshellcmd(htmlentities($_POST['id'])));
+require_once addslashes(htmlentities($_POST['id']));
